8e02e75ac2d497090b0c6e6e3e5c2942caf5a9f2
Kasper Tvede <kasper@webmasteren.eu>
2025-06-09 17:14:38 +0200

    more tests

--- src/commonTest/kotlin/org/csenseoss/kotlin/tests/assertions/collections/array/typed/int/AssertTest.kt
+++ src/commonTest/kotlin/org/csenseoss/kotlin/tests/assertions/collections/array/typed/int/AssertTest.kt
@@ -5,58 +5,93 @@ import kotlin.test.*
 
 class AssertTest {
 
-    @Test
-    fun emptyToEmpty() {
-        intArrayOf().assert(intArrayOf())
+    class Assert {
+        @Test
+        fun emptyToEmpty() {
+            intArrayOf().assert(intArrayOf())
+        }
+
+        @Test
+        fun emptyToSingle() = assertThrows<Throwable> {
+            intArrayOf().assert(intArrayOf(1))
+        }
+
+        @Test
+        fun emptyToMultiple() = assertThrows<Throwable> {
+            intArrayOf().assert(intArrayOf(1, 2))
+        }
+
+        @Test
+        fun singleToEmpty() = assertThrows<Throwable> {
+            intArrayOf(2).assert(intArrayOf())
+        }
+
+        @Test
+        fun singleToSingleSame() {
+            intArrayOf(1).assert(intArrayOf(1))
+        }
+
+        @Test
+        fun singleToSingleDifferent() = assertThrows<Throwable> {
+            intArrayOf(1).assert(intArrayOf(2))
+        }
+
+        @Test
+        fun singleToMultiple() = assertThrows<Throwable> {
+            intArrayOf(1).assert(intArrayOf(1, 2))
+        }
+
+        @Test
+        fun multipleToEmpty() = assertThrows<Throwable> {
+            intArrayOf(1, 2).assert(intArrayOf())
+        }
+
+        @Test
+        fun multipleToSingle() = assertThrows<Throwable> {
+            intArrayOf(1, 2).assert(intArrayOf(1))
+        }
+
+        @Test
+        fun multipleToMultipleSame() {
+            intArrayOf(1, 2).assert(intArrayOf(1, 2))
+        }
+
+        @Test
+        fun multipleToMultipleDifferent() = assertThrows<Throwable> {
+            intArrayOf(1, 1).assert(intArrayOf(1, 2))
+        }
     }
 
-    @Test
-    fun emptyToSingle() = assertThrows<Throwable> {
-        intArrayOf().assert(intArrayOf(1))
-    }
 
-    @Test
-    fun emptyToMultiple() = assertThrows<Throwable> {
-        intArrayOf().assert(intArrayOf(1, 2))
-    }
+    class AssertVararg {
+        @Test
+        fun emptyToEmpty() {
+            intArrayOf().assert()
+        }
 
-    @Test
-    fun singleToEmpty() = assertThrows<Throwable> {
-        intArrayOf(2).assert(intArrayOf())
-    }
+        @Test
+        fun contentToEmpty(): Unit = assertThrows<Throwable> {
+            intArrayOf(42).assert()
+        }
 
-    @Test
-    fun singleToSingleSame() {
-        intArrayOf(1).assert(intArrayOf(1))
-    }
+        @Test
+        fun singleToSingleMatching() {
+            intArrayOf(42).assert(42)
+        }
 
-    @Test
-    fun singleToSingleDifferent() = assertThrows<Throwable> {
-        intArrayOf(1).assert(intArrayOf(2))
-    }
+        @Test
+        fun singleToSingleMisMatching(): Unit = assertThrows<Throwable> {
+            intArrayOf(42).assert(11)
+        }
 
-    @Test
-    fun singleToMultiple() = assertThrows<Throwable> {
-        intArrayOf(1).assert(intArrayOf(1, 2))
-    }
+        @Test
+        fun multipleMismatching(): Unit = assertThrows<Throwable> {
+            intArrayOf(42, 22, 33).assert(42, 11, 33)
+        }
 
-    @Test
-    fun multipleToEmpty() = assertThrows<Throwable> {
-        intArrayOf(1, 2).assert(intArrayOf())
-    }
-
-    @Test
-    fun multipleToSingle() = assertThrows<Throwable> {
-        intArrayOf(1, 2).assert(intArrayOf(1))
-    }
-
-    @Test
-    fun multipleToMultipleSame() {
-        intArrayOf(1, 2).assert(intArrayOf(1, 2))
-    }
-
-    @Test
-    fun multipleToMultipleDifferent() = assertThrows<Throwable> {
-        intArrayOf(1, 1).assert(intArrayOf(1, 2))
+        @Test
+        fun multipleMatching() {
+            intArrayOf(42,11,45).assert(42, 11, 45)
+        }
     }
 }
